38fa61b85a3201e6bd9968e7abd08c39139360bf
Дмитрий Коннов <konnov@outlook.com>
2020-04-09 10:19:20 +0900

    lesson 43

--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
-const {Schema, model} = require('mongoose')
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
   email: {
     type: String,
-    required: true
+    required: true,
   },
   name: {
     type: String,
-    required: true
+    required: true,
   },
   cart: {
     items: [
@@ -15,16 +15,34 @@ const userSchema = new Schema({
         count: {
           type: Number,
           required: true,
-          default: 1
+          default: 1,
         },
         courseId: {
           type: Schema.Types.ObjectId,
-          ref: 'Course',
-          required: true
-        }
-      }
-    ]
-  }
-})
+          ref: "Course",
+          required: true,
+        },
+      },
+    ],
+  },
+});
 
-module.exports = model('User', userSchema)
+userSchema.methods.addToCart = function (course) {
+  const items = [...this.cart.items];
+  const idx = items.findIndex((c) => {
+    return c.courseId.toStrind() === course._id.toString();
+  });
+  if (idx >= 0) {
+    items[idx].count++;
+  } else {
+    items.push({
+      courseId: course._id,
+      count: 1,
+    });
+  }
+
+  this.cart = { items };
+  return this.save();
+};
+
+module.exports = model("User", userSchema);
