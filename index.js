3e045dc96bdd0a32d779b9a456830f7f9bcd68a1
Дмитрий Коннов <konnov@outlook.com>
2020-04-13 10:29:33 +0900

    lesson 50

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cardRoutes = require("./routes/card");
 const addRoutes = require("./routes/add");
 const ordersRoutes = require("./routes/orders.js");
 const coursesRoutes = require("./routes/courses");
+const authRoutes = require("./routes/auth");
 const User = require("./models/user");
 
 var Handlebars = require("handlebars");
@@ -44,6 +45,7 @@ app.use("/add", addRoutes);
 app.use("/courses", coursesRoutes);
 app.use("/card", cardRoutes);
 app.use("/orders", ordersRoutes);
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
