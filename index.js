a0fb6ec4bdbad078bed88ff4bd607f0a6ad9f13c
Дмитрий Коннов <konnov@outlook.com>
2020-04-13 16:18:26 +0900

    lesson 52

--- index.js
+++ index.js
@@ -1,76 +1,80 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const exphbs = require("express-handlebars");
-const homeRoutes = require("./routes/home");
-const cardRoutes = require("./routes/card");
-const addRoutes = require("./routes/add");
-const ordersRoutes = require("./routes/orders.js");
-const coursesRoutes = require("./routes/courses");
-const authRoutes = require("./routes/auth");
-const User = require("./models/user");
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
+const coursesRoutes = require('./routes/courses')
+const authRoutes = require('./routes/auth')
+const User = require('./models/user')
+const varMiddleware = require('./middleware/variables')
 
-var Handlebars = require("handlebars");
-var {
-  allowInsecurePrototypeAccess,
-} = require("@handlebars/allow-prototype-access");
-
-const app = express();
+const app = express()
 
 const hbs = exphbs.create({
-  defaultLayout: "main",
-  extname: "hbs",
-  handlebars: allowInsecurePrototypeAccess(Handlebars),
-});
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
 
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
-app.set("views", "views");
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
 app.use(async (req, res, next) => {
   try {
-    const user = await User.findById("5e8d21212bae29bfb8e426ed");
-    req.user = user;
-    next();
+    const user = await User.findById('5cc1d29dcedab01481e03660')
+    req.user = user
+    next()
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
-});
+})
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(varMiddleware)
 
-app.use("/", homeRoutes);
-app.use("/add", addRoutes);
-app.use("/courses", coursesRoutes);
-app.use("/card", cardRoutes);
-app.use("/orders", ordersRoutes);
-app.use("/auth", authRoutes);
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 async function start() {
   try {
-    const url = `mongodb+srv://dmitry:lwFrKrZxKYMV46hw@cluster0-gfhyj.mongodb.net/shop`;
+    const url = `mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop`
     await mongoose.connect(url, {
       useNewUrlParser: true,
-      useFindAndModify: false,
-    });
-    const candidate = await User.findOne();
+      useFindAndModify: false
+    })
+    const candidate = await User.findOne()
     if (!candidate) {
       const user = new User({
-        email: "konnov@outlook.com",
-        name: "Dmitry",
-        cart: { items: [] },
-      });
-      await user.save();
+        email: 'vladilen@mail.ru',
+        name: 'Vladilen',
+        cart: {items: []}
+      })
+      await user.save()
     }
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+      console.log(`Server is running on port ${PORT}`)
+    })
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
 }
 
-start();
+start()
+
+
