71fd540e3d6af3b7bd8c9bdae2340f9186580cc1
Дмитрий Коннов <konnov@outlook.com>
2020-04-14 14:08:08 +0900

    lesson 54

--- routes/card.js
+++ routes/card.js
@@ -1,51 +1,50 @@
-const {Router} = require('express')
-const Course = require('../models/course')
-const router = Router()
-
+const { Router } = require("express");
+const Course = require("../models/course");
+const auth = require("../middleware/auth");
+const router = Router();
 
 function mapCartItems(cart) {
-  return cart.items.map(c => ({
-    ...c.courseId._doc, 
+  return cart.items.map((c) => ({
+    ...c.courseId._doc,
     id: c.courseId.id,
-    count: c.count
-  }))
+    count: c.count,
+  }));
 }
 
 function computePrice(courses) {
   return courses.reduce((total, course) => {
-    return total += course.price * course.count
-  }, 0)
+    return (total += course.price * course.count);
+  }, 0);
 }
 
-router.post('/add', async (req, res) => {
-  const course = await Course.findById(req.body.id)
-  await req.user.addToCart(course)
-  res.redirect('/card')
-})
+router.post("/add", auth, async (req, res) => {
+  const course = await Course.findById(req.body.id);
+  await req.user.addToCart(course);
+  res.redirect("/card");
+});
 
-router.delete('/remove/:id', async (req, res) => {
-  await req.user.removeFromCart(req.params.id)
-  const user = await req.user.populate('cart.items.courseId').execPopulate()
-  const courses = mapCartItems(user.cart)
+router.delete("/remove/:id", auth, async (req, res) => {
+  await req.user.removeFromCart(req.params.id);
+  const user = await req.user.populate("cart.items.courseId").execPopulate();
+  const courses = mapCartItems(user.cart);
   const cart = {
-    courses, price: computePrice(courses)
-  }
-  res.status(200).json(cart)
-})
+    courses,
+    price: computePrice(courses),
+  };
+  res.status(200).json(cart);
+});
 
-router.get('/', async (req, res) => {
-  const user = await req.user
-    .populate('cart.items.courseId')
-    .execPopulate()
+router.get("/", auth, async (req, res) => {
+  const user = await req.user.populate("cart.items.courseId").execPopulate();
 
-  const courses = mapCartItems(user.cart)
+  const courses = mapCartItems(user.cart);
 
-  res.render('card', {
-    title: 'Корзина',
+  res.render("card", {
+    title: "Корзина",
     isCard: true,
     courses: courses,
-    price: computePrice(courses)
-  })
-})
+    price: computePrice(courses),
+  });
+});
 
-module.exports = router
+module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -1,55 +1,55 @@
-const {Router} = require('express')
-const Course = require('../models/course')
-const router = Router()
+const { Router } = require("express");
+const Course = require("../models/course");
+const router = Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const courses = await Course.find()
-    .populate('userId', 'email name')
-    .select('price title img')
+    .populate("userId", "email name")
+    .select("price title img");
 
-  res.render('courses', {
-    title: 'Курсы',
+  res.render("courses", {
+    title: "Курсы",
     isCourses: true,
-    courses
-  })
-})
+    courses,
+  });
+});
 
-router.get('/:id/edit', async (req, res) => {
+router.get("/:id/edit", auth, async (req, res) => {
   if (!req.query.allow) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
 
-  const course = await Course.findById(req.params.id)
+  const course = await Course.findById(req.params.id);
 
-  res.render('course-edit', {
+  res.render("course-edit", {
     title: `Редактировать ${course.title}`,
-    course
-  })
-})
+    course,
+  });
+});
 
-router.post('/edit', async (req, res) => {
-  const {id} = req.body
-  delete req.body.id
-  await Course.findByIdAndUpdate(id, req.body)
-  res.redirect('/courses')
-})
+router.post("/edit", auth, async (req, res) => {
+  const { id } = req.body;
+  delete req.body.id;
+  await Course.findByIdAndUpdate(id, req.body);
+  res.redirect("/courses");
+});
 
-router.post('/remove', async (req, res) => {
+router.post("/remove", auth, async (req, res) => {
   try {
-    await Course.deleteOne({_id: req.body.id})
-    res.redirect('/courses')
+    await Course.deleteOne({ _id: req.body.id });
+    res.redirect("/courses");
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-})
+});
 
-router.get('/:id', async (req, res) => {
-  const course = await Course.findById(req.params.id)
-  res.render('course', {
-    layout: 'empty',
+router.get("/:id", async (req, res) => {
+  const course = await Course.findById(req.params.id);
+  res.render("course", {
+    layout: "empty",
     title: `Курс ${course.title}`,
-    course
-  })
-})
+    course,
+  });
+});
 
-module.exports = router
+module.exports = router;
