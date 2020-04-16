35f1c09508781b96ce714b2a31e1206fdc79fb40
Дмитрий Коннов <konnov@outlook.com>
2020-04-16 15:33:34 +0900

    lesson 62

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const coursesRoutes = require("./routes/courses");
 const authRoutes = require("./routes/auth");
 const varMiddleware = require("./middleware/variables");
 const userMiddleware = require("./middleware/user");
+const keys = require("./keys");
 
-const MONGODB_URI = `mongodb+srv://dmitry:lwFrKrZxKYMV46hw@cluster0-gfhyj.mongodb.net/shop`;
 const app = express();
 const hbs = exphbs.create({
   defaultLayout: "main",
@@ -23,7 +23,7 @@ const hbs = exphbs.create({
 });
 const store = new MongoStore({
   collection: "sessions",
-  uri: MONGODB_URI,
+  uri: keys.MONGODB_URI,
 });
 
 app.engine("hbs", hbs.engine);
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: "some secret value",
+    secret: keys.secret,
     resave: false,
     saveUninitialized: false,
     store,
@@ -56,7 +56,7 @@ const PORT = process.env.PORT || 3000;
 
 async function start() {
   try {
-    await mongoose.connect(MONGODB_URI, {
+    await mongoose.connect(keys.MONGODB_URI, {
       useNewUrlParser: true,
       useFindAndModify: false,
     });
---
+++ keys/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB_URI: `mongodb+srv://dmitry:lwFrKrZxKYMV46hw@cluster0-gfhyj.mongodb.net/shop`,
+  secret: "some secret value",
+};
