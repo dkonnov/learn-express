8ebc965697b720f7b54b0d763caa657463163001
Дмитрий Коннов <konnov@outlook.com>
2020-04-21 13:00:18 +0900

    lesson 72

--- routes/auth.js
+++ routes/auth.js
@@ -65,8 +65,7 @@ router.post("/login", async (req, res) => {
 
 router.post("/register", registerValidators, async (req, res) => {
   try {
-    const { email, password, confirm, name } = req.body;
-    const candidate = await User.findOne({ email });
+    const { email, password, name } = req.body;
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -74,21 +73,16 @@ router.post("/register", registerValidators, async (req, res) => {
       return res.status(422).redirect("/auth/login#register");
     }
 
-    if (candidate) {
-      req.flash("registerError", "Пользователь с таким email уже сущестует");
-      res.redirect("/auth/login#register");
-    } else {
-      const hashPassword = await bcrypt.hash(password, 10);
-      const user = new User({
-        email,
-        name,
-        password: hashPassword,
-        cart: { items: [] },
-      });
-      await user.save();
-      res.redirect("/auth/login#login");
-      await transporter.sendMail(regMail(email));
-    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      email,
+      name,
+      password: hashPassword,
+      cart: { items: [] },
+    });
+    await user.save();
+    res.redirect("/auth/login#login");
+    await transporter.sendMail(regMail(email));
   } catch (e) {
     console.log(e);
   }
--- utils/validators.js
+++ utils/validators.js
@@ -1,18 +1,30 @@
-const { body } = require("express-validator");
+const { body } = require("express-validator/check");
+const User = require("../models/user");
 
 exports.registerValidators = [
-  body("email").isEmail().withMessage("Ввидете корректную форму"),
-  body("password", "Пароль должен быть 6 символов").isLength({
-    min: 6,
-    max: 56,
-  }).isAlphanumeric,
+  body("email")
+    .isEmail()
+    .withMessage("Введите корректный email")
+    .custom(async (value, { req }) => {
+      try {
+        const user = await User.findOne({ email: value });
+        if (user) {
+          return Promise.reject("Такой email уже занят");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }),
+  body("password", "Пароль должен быть минимум 6 символов")
+    .isLength({ min: 6, max: 56 })
+    .isAlphanumeric(),
   body("confirm").custom((value, { req }) => {
     if (value !== req.body.password) {
-      throw new Error("Пароли олжны совпадать");
+      throw new Error("Пароли должны совпадать");
     }
     return true;
   }),
   body("name")
     .isLength({ min: 3 })
-    .withMessage("Имя должно быть не меньше 3х символов"),
+    .withMessage("Имя должно быть минимум 3 символа"),
 ];
