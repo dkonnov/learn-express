59cd33f48fbb4dc57ae4b2ce964f3556a57506c4
Дмитрий Коннов <konnov@outlook.com>
2020-04-25 19:50:42 +0900

    lesson 79

--- keys/index.js
+++ keys/index.js
@@ -1,8 +1,5 @@
-module.exports = {
-  MONGODB_URI: `mongodb+srv://dmitry:lwFrKrZxKYMV46hw@cluster0-gfhyj.mongodb.net/shop`,
-  secret: "some secret value",
-  SENDGRID_API_KEY:
-    "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",
-  EMAIL_FROM: "konnov@outlook.com",
-  BASE_URL: "http://localhost:3000",
-};
+if (process.env.node_env === "production") {
+  module.exports = require("./keys.prod");
+} else {
+  module.exports = require("./keys.dev");
+}
